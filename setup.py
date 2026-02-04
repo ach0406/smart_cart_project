ee9bedcb1471cbfc93dc1886d3fb892288bac440
ach0406 <ach0406@hanyang.ac.kr>
2026-02-04 17:22:23 +0900

    Build: Complete ROS2 package structure with sensor_node

---
+++ setup.py
@@ -0,0 +1,8 @@
+entry_points={
+    'console_scripts': [
+        'motor_node = smart_cart_project.motor_node:main',
+        'qr_node = smart_cart_project.qr_node:main',
+        'planner = smart_cart_project.path_planner:main',
+        'sensor_node = smart_cart_project.sensor_node:main', 
+    ],
+},
